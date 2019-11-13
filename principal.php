b899e53ed5ed36ed21a0243c214547216ce79d06
Alejandro <alejomm1999@hotmail.com>
2019-11-13 13:56:48 -0500

    Pagina principal agregada

---
+++ principal.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LinkedLiving</title>
+    <meta charset="utf-8">
+</head>
+<body>
+	<img src="imagenes/Logo.png"/>
+    <h1> Información de los desarrolladores</h1>
+    <li>Santiago Zuñiga Navarro: Estudiante de sexto semestre de ingeniería de sistemas y ciencias de la computación en la Universidad del Norte</li>
+    <li>Andres Betancourt Vega: Estudiante de sexto semestre de ingeniería de sistemas y ciencias de la computación en la Universidad del Norte.</li>
+    <li>Alejandro Manotas Marmolejo: Estudiante de sexto semestre de ingeniería de sistemas y ciencias de la computación en la Universidad del Norte.
+        Estudiante de primer semestre de física pura en la Universidad del Átlantico. Técnico en análisis de muestras químicas de laboratorio egresado del SENA. </li>
+    <li>Carla Duran Mariota:</li>
+    <h2>LinkedLiving</h2>
+    <p>Desde hace algún tiempo hemos notado esta problemática en las universidades de la ciudad de Barranquilla, nos centraremos en las que pensamos que serían más rentable para la idea de negocios que hemos establecido. Nos hemos reunido un grupo de ingenieros de sistemas y hemos decidido desarrollar un proyecto llamado Linkedliving donde queremos brindar la oportunidad a estudiantes de cualquier tipo (becado, no becado, extranjero, etc.) de poder brindarle un lugar donde vivir cerca de la universidad de una forma confiable, cómoda y con una movilidad rápida a la hora de acceder al campus de cualquier universidad ubicada en la ciudad de Barranquilla. La idea de negocios es edificar una inmobiliaria de vivienda web que beneficie a las personas que no vivan en la ciudad de Barranquilla, justo después de presentar la idea de negocios entramos nosotros los ingenieros de sistemas para desarrollar la página web de fácil acceso y amigable al usuario para que cada persona pueda inscribirse, instalar, pagar, y vivir de forma cómoda, rápida, y fácil. Linkedliving es una aplicación de vivienda que ayuda a los estudiantes a encontrar un lugar donde hospedarse cerca de cualquier universidad en la ciudad de Barranquilla.</p>
+    <h3>Dirigido a:</h3>
+    <p>Queremos que Linkedliving se venda más como marca que como producto, se vende como cosmovisión de vida. El mundo ético de Linkedliving es el de estudiantes felices, joven que no se hace problema por tonterías y que es realmente como es y que en cierta forma se desata de la subjetivación social y se define por sí misma y no como lo definiría la sociedad. Genera identificación, una forma de ser dónde queremos estar. Comprando el producto se puede adoptar ese estilo de vida, pero sin engañar; la publicidad sugiere que el producto te acerca a ese estilo de vida ideal. Linkedliving va dirigido a todo el público que desee encontrar comodidad y calidad de vida, solo o acompañado, relajado o divertido, pero siempre sereno y fresco. Sin embargo, cabe destacar que la población a la que se dirige con más consistencia es a adolescentes y adultos jóvenes en un rango de edad entre 15 y 28 años</p>
+    <h3>información de contacto</h3>
+    <li>Santiago Zuñiga Navarro: sazuñiga@uninorte.edu.co. Telefono: + 57 3014466241</li>
+    <li>Andres Betancourt Vega: andresbetancourt@uninorte.edu.co. Telefono: +57 3024449506</li>
+    <li>Alejandro Manotas Marmolejo: manotasja@uninorte.edu.co. Telefono: +57 3122850191</li>
+    <li>Carla Duran Mariota: cmariota@uninorte.edu.co. Telefono: +57 3005421699</li>
+</body>
+</html>
